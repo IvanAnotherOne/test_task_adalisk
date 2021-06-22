9d4808a0a9a2f65b8beb2710b780d96b86b7af23
Ivan Artyukh <ivanartux12@gmail.com>
2021-06-22 10:46:02 +0300

    Apply Cuthill-McKee reordering algorithm to the loaded graph

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_matrix.hpp>
+#include <boost/graph/cuthill_mckee_ordering.hpp>
+#include <boost/compute/iterator/transform_iterator.hpp>
 #include "jsonGraphIO.h"
 #include "stdGraphIO.h"
 #include "gphGraphIO.h"
@@ -42,6 +44,7 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    // Get input/output graph files from arguments
     std::unique_ptr<GraphIO<Graph>> input, output;
     try {
         // Get input
@@ -59,8 +62,29 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    // Now load graph
     Graph g = input->load();
-    output->dump(g);
+
+    // Apply Cuthill-McKee ordering algorithm
+    typedef graph_traits<Graph>::vertex_descriptor Vertex;
+    std::vector<Vertex> perm(num_vertices(g));
+    cuthill_mckee_ordering(g, perm.begin());
+
+    // Create new graph with reordered vertices
+    typedef property_map<Graph, vertex_index_t>::const_type VertexIndexMap;
+    typedef graph_traits<Graph>::edge_descriptor Edge;
+    VertexIndexMap index_map = get(vertex_index, g);
+    iterator_property_map <Vertex *, VertexIndexMap> perm_map(perm.data(), index_map);
+    auto g_edges = edges(g);
+    const auto perm_edge = [&g, &perm_map](const Edge &e) {
+        return std::make_pair( get(perm_map, source(e, g)), get(perm_map, target(e, g)) );
+    };
+    Graph g_permuted(
+              make_transform_iterator(g_edges.first, perm_edge),
+              make_transform_iterator(g_edges.second, perm_edge),
+              num_vertices(g));
+    // And finally save it
+    output->dump(g_permuted);
 
     return 0;
 }
